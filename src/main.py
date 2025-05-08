81e192cb7d53561e8b9a36a591c410c91a514ce9
Paul <paulkaeb@gmx.de>
2025-05-08 13:21:15 +0200

    html and leaf nodes done

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 from textnode import *
+from htmlnode import *
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(node)
+    #node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    #print(node)
 
+    html_node =  HTMLNode("p","text text lol", None,{"class": "greeting", "href": "https://boot.dev"})
+    print(html_node.props_to_html())
+    print(html_node)
     return
 
 main()
